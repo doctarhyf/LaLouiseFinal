31a87f0fb3fc4626bf1a2d1206bd3cab712380c8
Docta Rhyf <drrhyf@gmail.com>
2023-01-24 23:31:02 +0000

    working on login page ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "./styles.css";
-import { Container, Button, Box, Typography, TextField } from "@mui/material";
+import { useState } from "react";
+import {
+  Container,
+  Button,
+  Box,
+  Typography,
+  TextField,
+  Link
+} from "@mui/material";
 import logo from "./assets/logo.jpg";
 
 export default function App() {
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = (e) => {
+    alert(e);
+  };
+
+  const onPasswordLost = (e) => {
+    alert(e);
+  };
+
   return (
     <Container sx={{ p: 4 }}>
       <Box sx={{ gap: 4, display: "flex", flexDirection: "column" }}>
@@ -17,15 +36,27 @@ export default function App() {
             sx={{
               width: 306,
               height: 270,
-              backgroundColor: "tomato",
               alignSelf: { xs: "center", md: "left" }
             }}
           >
             <img src={logo} alt="La Louise" />
           </Box>
           <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
-            <TextField id="filled-basic" label="Phone" variant="filled" />
-            <TextField id="filled-basic" label="Password" variant="filled" />
+            <TextField
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              id="filled-basic"
+              label="Phone"
+              variant="filled"
+            />
+            <TextField
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="filled-basic"
+              label="Password"
+              variant="filled"
+            />
             <Typography sx={{ textAlign: "center" }}>
               Bienvenue sur le systeme de gestion de LaLouise . Veuillez vous
               connectez avec votre numero de telephone et votre mot de passe
@@ -34,8 +65,34 @@ export default function App() {
         </Box>
 
         <Box sx={{ display: "flex", gap: 2, flexDirection: "column" }}>
-          <Button variant="contained">SE CONNECTER</Button>
-          <Button color="error">Mot de passe oublie?</Button>
+          <Button
+            onClick={onLogin}
+            variant="contained"
+            sx={{ width: 1 / 3, alignSelf: "center" }}
+          >
+            SE CONNECTER
+          </Button>
+          <Button
+            onClick={onPasswordLost}
+            color="error"
+            sx={{ width: 1 / 3, alignSelf: "center" }}
+          >
+            Mot de passe oublie?
+          </Button>
+        </Box>
+
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            pb: 2,
+            pt: 4,
+            alignSelf: "center"
+          }}
+        >
+          <Typography>
+            <Link>@DoctaRhyf</Link> 2023
+          </Typography>
         </Box>
       </Box>
     </Container>
