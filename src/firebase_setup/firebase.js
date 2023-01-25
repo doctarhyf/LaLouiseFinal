a26863180f4733bb52c5b318b50dfd20f5086447
Docta Rhyf <drrhyf@gmail.com>
2023-01-25 08:02:51 +0000

    added firebase

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
+  apiKey: "AIzaSyCKzgxyaT9PZxPvq-WDESCnTyV6Ap0E_A8",
+  authDomain: "lalouise-6ef7b.firebaseapp.com",
+  projectId: "lalouise-6ef7b",
+  storageBucket: "lalouise-6ef7b.appspot.com",
+  messagingSenderId: "843021144425",
+  appId: "1:843021144425:web:e2f0d3e3cdbd86f7d3edf8",
+  measurementId: "G-7DGT07PB1Y"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/handles/handlesubmit.js
+++ src/handles/handlesubmit.js
@@ -1,5 +1,6 @@
 import { addDoc, collection } from "@firebase/firestore";
 import { firestore } from "../firebase_setup/firebase";
+
 const handleSubmit = (testdata) => {
   const ref = collection(firestore, "test_data"); // Firebase creates this automatically
   let data = {
@@ -7,8 +8,10 @@ const handleSubmit = (testdata) => {
   };
   try {
     addDoc(ref, data);
+    alert(`adding to fb ${JSON.stringify(ref)}`);
   } catch (err) {
     console.log(err);
+    alert(err);
   }
 };
 export default handleSubmit;
